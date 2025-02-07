8e2616ede3aefe1c2e540a697e750440cacfb67c
YeswanthB1101 <yeswanthyes333@gmail.com>
2025-02-07 15:06:25 +0530

    BaseDriver Setup

--- src/test/java/Utility/BaseDriver.java
+++ src/test/java/Utility/BaseDriver.java
@@ -3,12 +3,44 @@ package Utility;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class BaseDriver {
-	private WebDriver driver;
+	private final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+	private final ThreadLocal<String> browser = new ThreadLocal<>();
 
-	public void superclass() {
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-
+	private BaseDriver() {
 	}
+	@BeforeClass
+	public WebDriver getDriver() {
+		if (browser.get() == null) {
+			browser.set("chrome");
+		}
+
+		if (driver.get() == null) {
+			switch(browser.get()){
+				case "edge":
+					driver.set(new EdgeDriver());
+				case "firefox":
+					driver.set(new FirefoxDriver());
+				default:
+					driver.set(new ChromeDriver());
+			}
+
+			driver.get().manage().window().maximize();
+			driver.get().manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
+
+		}
+		return null;
+	}
+
+	@AfterClass
+	public void driverQuit(){
+		if(driver.get()!=null) driver.get().quit;
+	}
+
+	public static void main(String args[]){
+		getDriver;
+	}
+
 }
